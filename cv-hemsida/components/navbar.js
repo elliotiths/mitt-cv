4ceb55139b85fc9c574fcdf8f7e338cf99fcdaf9
elliotiths <elliot.axelsson@iths.se>
2025-12-16 12:03:12 +0100

    Add files via upload
    added vue-component

---
+++ cv-hemsida/components/navbar.js
@@ -0,0 +1,12 @@
+const NavBar = {
+  template: `
+    <nav>
+      <ul>
+        <li><a href="index.html">Hem</a></li>
+        <li><a href="om-mig.html">Om mig</a></li>
+        <li><a href="projekt.html">Mina projekt</a></li>
+        <li><a href="kontakt.html">Kontakt</a></li>
+      </ul>
+    </nav>
+  `,
+};
